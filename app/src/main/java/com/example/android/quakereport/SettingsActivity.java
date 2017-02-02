5e0fac95cfd77de40d1d3df5f0f72e9b5e96ca84
Pao <jzarsuelo.dev@gmail.com>
2017-02-02 12:41:00 +0800

    [lesson4] - Added preference summary

--- app/src/main/java/com/example/android/quakereport/SettingsActivity.java
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.quakereport;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -32,6 +34,13 @@ public class SettingsActivity extends AppCompatActivity {
             String stringValue = value.toString();
             preference.setSummary(stringValue);
             return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
